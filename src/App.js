9aa7b1c6d75079ab2b1927779212e9187b82ddb4
Darren Z <darren.zagrean@gmail.com>
2024-08-23 22:29:09 -0700

    walter white add on

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+import myImage from './waltuh.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myImage} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Yes!!
+          Edit <code>src/App.js</code> and save to reload. yoooo mr white
         </p>
         <a
           className="App-link"
