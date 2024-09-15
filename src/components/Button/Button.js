e46c66060c165151d58767b5c7d60a02310ff913
Alexandru Pop <alexandru.pop@sfappworks.com>
2024-09-15 22:29:03 +0300

    Update styling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,11 +3,9 @@ import './style.css';  // This is where you'll define the button's styles
 
 const Button = ({ onClick, children }) => {
     return (
-        <div>
-            <button className="basic-button" onClick={onClick}>
-                {children}
-            </button>
-        </div>
+        <button className="basic-button" onClick={onClick}>
+            {children}
+        </button>
     );
 };
 
