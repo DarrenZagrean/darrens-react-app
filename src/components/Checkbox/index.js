ba439320aba67bde0b50618298d929a8fa78ccba
Alexandru Pop <alexandru.pop@sfappworks.com>
2024-10-20 21:29:55 +0300

    Font family for everything, align checkboxes for todo board

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -1,20 +1,17 @@
+import Input from "../Input";
 import React from 'react';
 
-const Checkbox = ({ task, onToggle }) => {
-  return React.createElement(
-    'div',
-    { style: { marginBottom: '10px' } },
-    React.createElement('input', {
-      type: 'checkbox',
-      checked: task.isChecked,
-      onChange: () => onToggle(task.id)
-    }),
-    React.createElement(
-      'span',
-      { style: { textDecoration: task.isChecked ? 'line-through' : 'none' } },
-      task.name
-    )
-  );
+const Checkbox = ({task, onToggle}) => {
+    return <div style={{
+        marginBottom: '10px',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'start'
+    }}>
+        <Input type={"checkbox"} checked={task.isChecked} onChange={() => onToggle(task.id)}></Input>
+        <span style={{textDecoration: task.isChecked ? 'line-through' : 'none'}}>{task.name}</span>
+    </div>
 };
 
 export default Checkbox;
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import './style.css'
 
-const Input = ({className, type, value, onChange}) => {
+const Input = ({className, type, value, onChange, ...props}) => {
     return (
         <input
             className={className}
             type={type}
             value={value}
             onChange={onChange}
+            {...props}
         />
     );
 };
