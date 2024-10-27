2666281c4d1563c8a0d6ace396d32f98147d1b15
Darren Z <darren.zagrean@gmail.com>
2024-10-27 12:00:17 -0700

    home page component

--- src/App.js
+++ src/App.js
@@ -25,17 +25,6 @@ function App() {
   );
 }
 
-function HomePage() {
-  return (
-    <div className="home-container">
-      <h1>Welcome to the To-Do App</h1>
-      <p>An organizational tool</p>
-      <div className="home-links">
-        <Link to="/users" className="home-button">Go to User Page</Link>
-      </div>
-    </div>
-  );
-}
 
 
 export default App;
